b9ae712b8f2f8ac7c3a1082b2f72289295287cee
aviuk <aviuk@192.168.0.162>
2020-06-12 14:21:24 +0200

    New Commit: Learned Timertask, and Random Number Generation
    Next, Obtain User Input

--- heloworld/src/com/heloworld/helloworld.java
+++ heloworld/src/com/heloworld/helloworld.java
@@ -1,19 +1,33 @@
 package com.heloworld;
+import java.util.Random;
 import java.util.Timer;
 import java.util.TimerTask;
 
+//Timer Task is invoked
+//Integer Generated between 0 and 36
 class Demo extends TimerTask {
     public void run() {
-          System.out.println("Hello World"); 
+    	int rand=0, min=0, max = 36;
+    	    Random r = new Random();
+    	    rand = r.nextInt((max - min) + 1) + min;	    
+    	    System.out.println(rand); 
+          
      }
 }
 
+
+
 public class helloworld {
 
+
+
+	
     public static void main(String args[]) {
-    	Timer t1 = new Timer();
-    	t1.schedule(new Demo(), 0,5000);
     	
+    	
+    	Timer t1 = new Timer();
+    	//Timer Scheduled for every 4 seconds
+    	t1.schedule(new Demo(), 0,4000);
         
     }
 }
