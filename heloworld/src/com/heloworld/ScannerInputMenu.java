361351f7f9047ebaced075085113cc86e274ff82
aviuk <aviuk@192.168.0.162>
2020-06-14 06:23:12 +0200

    New Commit - Learning about threads. Added new java class file to work on keyboard input, and creating console menu

---
+++ heloworld/src/com/heloworld/ScannerInputMenu.java
@@ -0,0 +1,10 @@
+package com.heloworld;
+
+public class ScannerInputMenu {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello");
+	}
+
+}
--- heloworld/src/com/heloworld/helloworld.java
+++ heloworld/src/com/heloworld/helloworld.java
@@ -3,16 +3,13 @@ import java.util.Random;
 import java.util.Timer;
 import java.util.TimerTask;
 
-import com.sun.java_cup.internal.runtime.Scanner;
-
-//Timer Task is invoked
-//Integer Generated between 0 and 36
+//Timer t
 class Demo extends TimerTask {
     public void run() {
     	int rand=0, min=0, max = 36;
     	    Random r = new Random();
     	    rand = r.nextInt((max - min) + 1) + min;	    
-    	    System.out.println(rand); 
+    	    System.out.println(rand); // Roulette Number
           
      }
 }
@@ -20,28 +17,15 @@ class Demo extends TimerTask {
 
 
 public class helloworld {
-	
-	 public class KeyboardInput extends Thread{
-	        Scanner sc= new Scanner(System.in);
-	        @Override
-	        public void run()
-	        {
-	        while(true)
-	        {
-	        sc.hasNext();
-	        }
 
-	    }
 
-	}
-	
+
 	
     public static void main(String args[]) {
-    	readln    	
     	Timer t1 = new Timer();
+
     	//Timer Scheduled for every 4 seconds
     	t1.schedule(new Demo(), 0,4000);
         
     }
 }
-
