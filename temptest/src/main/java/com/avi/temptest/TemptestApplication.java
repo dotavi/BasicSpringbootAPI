7866ef6057d174bffa1d52b734cf1b680f8d0b98
dotavi <avi.ukzn@gmail.com>
2020-06-17 17:37:46 +0200

    Add files via upload
    Rebuilt the Spring boot Pom....recreated the HelloController. All Services now available when you run the service at 8080
    e.g. http://localhost:8080/conversions/ctok

---
+++ temptest/src/main/java/com/avi/temptest/TemptestApplication.java
@@ -0,0 +1,13 @@
+package com.avi.temptest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemptestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TemptestApplication.class, args);
+	}
+
+}
